669e0e2038a58f67e4cc300ccd380819effb21fd
Javier Heisecke <javier.heisecke@consultr.net>
2026-05-07 11:58:10 -0300

    fix: override swift-collections dependency to JHeisecke fork for Swift 6.2

--- Package.swift
+++ Package.swift
@@ -16,6 +16,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/JHeisecke/swift-log.git", branch: "fix/swift-6.2-library-evolution-inlinable"),
 		.package(url: "https://github.com/JHeisecke/eudi-lib-ios-iso18013-data-model.git", branch: "main"),
+        .package(url: "https://github.com/JHeisecke/swift-collections.git", branch: "main"),
 		],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
